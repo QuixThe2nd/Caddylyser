07bb5aea6f4d4e885514d13d78d000c2ac2de6ae
Parsa Yazdani <parsa@yazdani.au>
2024-01-12 22:41:02 +1100

    Update caddylyser.py

--- caddylyser.py
+++ caddylyser.py
@@ -1,12 +1,15 @@
 import json
-import asyncio
-import websockets
+import os
+from time import sleep
+from sys import stdout
 
 result = {}
+connected_clients = set()
 
 
 def flatten_object(obj, parent_key=''):
     items = []
+    obj.url = obj['request']['host'] + obj['request']['uri']
     for key, value in obj.items():
         new_key = f"{parent_key}.{key}" if parent_key else key
         if isinstance(value, dict):
@@ -25,60 +28,74 @@ def flatten_object(obj, parent_key=''):
     return dict(items)
 
 
-async def analyse_logs(last_ts=0):
+def output(message):
+    print(message)
+    stdout.flush()
+
+
+def analyse_logs(last_ts=0, start_line=0):
+    lines = []
     with open('access.log', 'r') as file:
-        lines = file.read().split('\n')
+        for i in range(start_line+100):
+            line = file.readline()
+            if not line:
+                break
+            lines.append(line)
 
     if len(lines[-1]) == 0:
         lines = lines[:-1]
 
     if last_ts:
-        lines = [line for line in lines if json.loads(line)['ts'] > last_ts]
+        valid_lines = []
+        for line in lines:
+            try:
+                data = json.loads(line)
+                if data['ts'] > last_ts:
+                    valid_lines.append(line)
+            except:
+                continue
+        lines = valid_lines
 
-    if len(lines) == 0:
-        await asyncio.sleep(1)
-        return await analyse_logs(last_ts)
+    if len(lines) == 0 or (len(lines) == 1 and len(lines[0]) == 0):
+        sleep(1)
+        return analyse_logs(last_ts, start_line)
 
-    i = 0
     for line in lines:
-        i += 1
-        if not line:
+        try:
+            data = json.loads(line)
+
+            if not line or len(line) == 0 or 'logger' not in data or data['logger'] != 'http.log.access':
+                continue
+
+            flatten_object(data)
+            output(json.dumps(result))
+        except Exception as e:
+            print(e)
             continue
 
-        data = json.loads(line)
+    new_save = {
+        'output': result if result else {},
+        'last_ts': json.loads(lines[-1])['ts'] if 'ts' in json.loads(lines[-1]) else last_ts,
+        'start_line': start_line+len(lines)
+    }
+    with open(os.path.dirname(__file__) + '/caddylyser.save', 'w') as file:
+        file.write(json.dumps(new_save))
 
-        if 'logger' not in data or data['logger'] != 'http.log.access':
-            continue
+    return analyse_logs(json.loads(lines[-1])['ts'], start_line+len(lines))
 
-        flatten_object(data)
 
-        result['backlog'] = len(lines) - i
-
-        await send_to_clients(result)
-
-    return await analyse_logs(json.loads(lines[-1])['ts'])
-
-connected_clients = set()
-
-async def send_to_clients(message):
-    if connected_clients:  # Check if there are any connected clients
-        await asyncio.wait([client.send(json.dumps(message)) for client in connected_clients])
-
-async def register(websocket):
-    connected_clients.add(websocket)
-
-async def unregister(websocket):
-    connected_clients.remove(websocket)
-
-async def server(websocket, path):
-    await register(websocket)
-    try:
-        await websocket.wait_closed()
-    finally:
-        await unregister(websocket)
-
-start_server = websockets.serve(server, "localhost", 5901)
-
-asyncio.get_event_loop().run_until_complete(start_server)
-asyncio.get_event_loop().run_until_complete(analyse_logs())
-asyncio.get_event_loop().run_forever()
+try:
+    if os.path.isfile(os.path.dirname(__file__) + '/caddylyser.save'):
+        with open(os.path.dirname(__file__) + '/caddylyser.save', 'r') as file:
+            try:
+                save = json.loads(file.read())
+                result = save['output']
+                restored_last_ts = save['last_ts']
+                restored_start_line = save['start_line']
+                analyse_logs(restored_last_ts, restored_start_line)
+            except:
+                analyse_logs()
+    else:
+        analyse_logs()
+except:
+    analyse_logs()
